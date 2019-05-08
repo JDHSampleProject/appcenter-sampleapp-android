ed39a354f002b04fb485b94eb2dd5570cc9eb67b
JDHSampleProject <jhufnagle@irthsolutions.com>
2019-05-08 16:06:18 -0400

    no

--- app/src/test/java/com/example/appcenter/sampleapp_android/ExampleUnitTest.java
+++ app/src/test/java/com/example/appcenter/sampleapp_android/ExampleUnitTest.java
@@ -5,7 +5,9 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.Rule;
 import org.junit.After;
+/**
 import com.microsoft.appcenter.espresso.*;
+* */
 
 /**
  * Example local unit test, which will execute on the development machine (host).
